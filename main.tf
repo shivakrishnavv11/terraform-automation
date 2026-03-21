a98d647afab68d70fe5fc13ab523953122d2a032
shivakrishnavv11 <shivakrishna.vv11@gmail.com>
2026-03-21 05:17:35 +0000

    fresh terraform project

---
+++ main.tf
@@ -0,0 +1,9 @@
+
+resource "aws_instance" "server" {
+  ami          = "ami-053b12d3152c0cc71"   # Amazon Linux Free tier AMI
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Manual-Terraform-Server"
+  }
+}
